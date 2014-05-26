79bc524eed8b5be0efba5c7cdab44ce54ce3223b
Sid Verma <mavidser@gmail.com>
2014-05-26 22:37:13 +0530

    code cleanup

--- main.py
+++ main.py
@@ -16,9 +16,7 @@ def get_data(query):
         app_secret = Config.get('Facebook','app_secret')
         fields     = 'object_id,id,from,story,picture,message,type,created_time,name,link,caption,properties,to'
         url        = 'https://graph.facebook.com/search?q='+query+'&type=post&key=value&access_token='+app_id+'|'+app_secret+'&fields='+fields
-        print 'url',url
         result     = requests.get(url)
-        # print result.text
         # return result
         q[0]       =result
 
@@ -28,10 +26,8 @@ def get_data(query):
         access_token        = Config.get('Twitter','access_token') # TODO: Get access token from server.
         access_token_secret = Config.get('Twitter','access_token_secret')
         url                 = 'https://api.twitter.com/1.1/search/tweets.json?q='+query
-        print 'url',url
         auth                = OAuth1(app_key,app_secret,access_token,access_token_secret)
         result              =  requests.get(url,auth=auth)
-        # print result.text
         # return result
         q[1]                =result
 
@@ -43,9 +39,7 @@ def get_data(query):
         ACCESSTOKEN  = Config.get('Instagram','ACCESSTOKEN') # TODO : Get access token from server.
         query        =query[3:]
         url          = 'https://api.instagram.com/v1/tags/'+query+'/media/recent?access_token='+ACCESSTOKEN
-        print 'url',url
         result       = requests.get(url)
-        # print result.text
         # return result
         q[2]         =result
     query = '%23'+query
@@ -67,10 +61,6 @@ def get_data(query):
     t1.join()
     t2.join()
     t3.join()
-    # for i in q:
-        # print i.json()
-
-    # print json.dumps(q[1].json())
 
     try :
         fb = q[0].json()
@@ -84,33 +74,19 @@ def get_data(query):
         ig = q[2].json()
     except:
         ig = {}
-    # print fb
-    # print ig
     fbdict = []
     instadict = []
     twitdict = []
-    print "fb",fb
-    print 'twit',twitter
-    print 'instagram',ig
     for i in fb['data']:
         q={}
-        # f.write(json.dumps(i))
-        # f.write('\n')
-        # f.write(i['link'])
-        # f.write('\n')
-        # f.write('https://facebook.com/'+i['id'].replace('_','/posts/'))
         q['id']='https://facebook.com/'+i['id'].replace('_','/posts/')
-        # f.write('\n')
         if i['type']=='status':
-            # f.write('status\t'+i['from']['name'].encode('utf8')+'\t'+'https://facebook.com/'+i['from']['id'].encode('utf8')+'\n')
             try:
                 q['story']=i['story']
-                # f.write(i['story'].encode('utf8') + '\n')
             except:
                 q['story']=''
             try:
                 q['message']=i['message']
-                # f.write(i['message'].encode('utf8') + '\n')
             except:
                 q['message']=''
             q['type']='status'
@@ -120,29 +96,23 @@ def get_data(query):
 
 
         elif i['type']=='link':
-            # f.write('link\t'+i['from']['name'].encode('utf8')+'\t'+'https://facebook.com/'+i['from']['id'].encode('utf8')+'\n')
             try:
-                # f.write(i['story'].encode('utf8') + '\n')
                 q['story']=i['story']
             except:
                 q['story']=''
             try:
-                # f.write(i['message'].encode('utf8') + '\n')
                 q['message']=i['message']
             except:
                 q['message']=''
             try:
-                # f.write(i['name'].encode('utf8') + '\n')
                 q['name']=i['name']
             except:
                 q['name']=''
             try:
-                # f.write(i['link'].encode('utf8') + '\n')
                 q['link']=i['link']
             except:
                 q['link']=''
             try:
-                # f.write(i['description'].encode('utf8') + '\n')
                 q['description']=i['description']
             except:
                 q['description']=''
@@ -151,20 +121,15 @@ def get_data(query):
             q['userhref']='https://facebook.com/'+i['from']['id']
 
         elif i['type']=='photo':
-            # f.write('photo\t'+i['from']['name'].encode('utf8')+'\t'+'https://facebook.com/'+i['from']['id'].encode('utf8')+'\n')
-            # f.write(i['picture']+'\n')
             try:
-                # f.write(i['story'].encode('utf8') + '\n')
                 q['story']=i['story']
             except:
                 q['story']=''
             try:
-                # f.write(i['message'].encode('utf8') + '\n')
                 q['message']=i['message']
             except:
                 q['message']=''
             try:
-                # f.write(i['caption'].encode('utf8') + '\n')
                 q['caption']=i['caption']
             except:
                 q['caption']=''
@@ -174,20 +139,15 @@ def get_data(query):
             q['picture']=i['picture']
 
         elif i['type']=='video':
-            # f.write('video\t'+i['from']['name'].encode('utf8')+'\t'+'https://facebook.com/'+i['from']['id'].encode('utf8')+'\n')
-            # f.write(i['picture']+'\n')
             try:
-                # f.write(i['story'].encode('utf8') + '\n')
                 q['story']=i['story']
             except:
                 q['story']=''
             try:
-                # f.write(i['message'].encode('utf8') + '\n')
                 q['message']=i['message']
             except:
                 q['message']=''
             try:
-                # f.write(i['description'].encode('utf8') + '\n')
                 q['description']=i['description']
             except:
                 q['description']=''
@@ -195,21 +155,11 @@ def get_data(query):
             q['user']=i['from']['name']
             q['userhref']='https://facebook.com/'+i['from']['id']
             q['picture']=i['picture']
-
-        # f.write('\n')
         fbdict.append(q)
-    # f.write('\n')
-    # f.write('previous page: '+fb['paging']['previous'])
-    # f.write('\n')
-    # f.write('next page: '+fb['paging']['next'])
-    # f.write('\n')
-
 
     try:
         for i in ig['data']:
             q={}
-            # print i
-            # print i['images']['low_resolution']['url']
             q['photo']=i['images']['low_resolution']['url']
             try:
                 q['caption']=i['caption']
@@ -223,25 +173,21 @@ def get_data(query):
 
     for i in twitter['statuses']:
         q={}
-        print i
         q['user']=i['user']['screen_name']
         q['text']=i['text']
         q['id']=i['id_str']
         try:
-            # print "yep",i['entities']['media'][0]
             q['picture']=i['entities']['media'][0]['media_url_https']
         except:
             q['picture']=''
         twitdict.append(q)
 
 
-    print fbdict
     return fbdict, instadict, twitdict
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # content = Markup(markdown.markdown(content))
     query = request.args.get('query')
     if query:
         query = ''.join(e for e in query if e.isalnum())
@@ -252,15 +198,3 @@ def index():
 def fb():
     fbdata,igdata,twdata = get_data('soccer')
     return render_template('fb.html', **locals())
-# @app.route('/')
-# def search():
-#   query = request.args.get('query')
-#   # get_data(query)
-#   # fbdata=fbdict
-#   # igdata=instadict
-#   # twdata=twitdict
-#   fbdata,igdata,twdata = get_data(query)
-#   # content = Markup(markdown.markdown(content))
-#   return render_template('search.html', **locals())
-
-# app.run(debug=True)
